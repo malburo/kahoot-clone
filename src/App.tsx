034f6500368727e684eccfe64b32ec0b19cc7258
malburo <tongquocbao0605@gmail.com>
2020-09-19 23:45:54 +0700

    Config globalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'antd/dist/antd.css';
 import RoutesComponent from './routes';
+import GlobalStyle from './globalStyles';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <RoutesComponent />
     </div>
   );
