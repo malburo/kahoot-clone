063410e12934f576fe0e5b1336659ba2565a368d
malburo <tongquocbao0605@gmail.com>
2020-09-21 16:43:40 +0700

    move getBase64 from QuizForm to utils

--- src/components/Form/QuizForm.tsx
+++ src/components/Form/QuizForm.tsx
@@ -1,3 +1,4 @@
+import getBase64 from '@/utils/image';
 import { Button, Col, Form, Input, Radio, Row, Upload } from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
 import React, { useState } from 'react';
@@ -9,14 +10,6 @@ const QuizForm = () => {
   const [previewVisible, setPreviewVisible] = useState(false);
   const [previewImage, setPreviewImage] = useState('');
   const [previewTitle, setPreviewTitle] = useState('');
-
-  const getBase64 = (file: any) =>
-    new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = error => reject(error);
-    });
 
   const handlePreview = async (file: any) => {
     const { name } = file;
