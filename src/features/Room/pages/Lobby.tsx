594e4cc93cd7a4c80ba5d789d8dc07c4d6dd6f87
malburo <tongquocbao0605@gmail.com>
2020-09-26 17:21:51 +0700

    feat/config-eslint

--- src/features/Room/pages/Lobby.tsx
+++ src/features/Room/pages/Lobby.tsx
@@ -2,41 +2,49 @@ import Box from '@/components/Common/Box';
 import Text from '@/components/Common/Text';
 import { UserOutlined } from '@ant-design/icons';
 import { Button, Col, Image, Row } from 'antd';
-import logo from '../../../assets/images/logo-white.png'
 import React from 'react';
+import logo from '../../../assets/images/logo-white.png';
 
 function Lobby() {
   return (
     <>
-      <Row justify="center" align="middle" style={{ height: "20vh", backgroundColor: "rgb(37, 7, 107)" }}>
+      <Row
+        justify="center"
+        align="middle"
+        style={{ height: '20vh', backgroundColor: 'rgb(37, 7, 107)' }}
+      >
         <Col>
           <Box backgroundColor="white" p="10px 20px">
             <Text fontSize={20}>Game Pin: 123123</Text>
           </Box>
         </Col>
       </Row>
-      <Row justify="space-between" align="middle" style={{ minHeight: "10vh", backgroundColor: "rgb(70, 23, 143)" }}>
+      <Row
+        justify="space-between"
+        align="middle"
+        style={{ minHeight: '10vh', backgroundColor: 'rgb(70, 23, 143)' }}
+      >
         <Col>
           <Box backgroundColor="rgba(0, 0, 0, 0.2)" color="white" p="10px 20px">
-            <Text fontSize={20}><UserOutlined /> 0</Text>
+            <Text fontSize={20}>
+              <UserOutlined /> 0
+            </Text>
           </Box>
         </Col>
         <Col>
-          <Image
-            src={logo}
-            width={96}
-            preview={false}
-          />
+          <Image src={logo} width={96} preview={false} />
         </Col>
         <Col>
           <Box mr={10}>
-            <Button>
-              Start
-          </Button>
+            <Button>Start</Button>
           </Box>
         </Col>
       </Row>
-      <Row justify="center" align="middle" style={{ minHeight: "70vh", backgroundColor: "rgb(70, 23, 143)" }}>
+      <Row
+        justify="center"
+        align="middle"
+        style={{ minHeight: '70vh', backgroundColor: 'rgb(70, 23, 143)' }}
+      >
         <Col>
           <Box backgroundColor="rgba(0, 0, 0, 0.2)" color="white" p="10px 20px">
             <Text fontSize={30}>Waiting for players…</Text>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import CreatorLayout from '@/components/layouts/CreatorLayout';
 import KahootLayout from '@/components/layouts/KahootLayout';
 import NotFound from '@/components/NotFound';
 import Auth from '@/features/Auth';
-// import Creator from '@/features/Kahoot/pages/Creator';
+import Creator from '@/features/Creator/Creator';
 import Kahoot from '@/features/Kahoot';
 import PlayGame from '@/features/PlayGame/PlayGame';
 import Room from '@/features/Room';
@@ -18,11 +18,11 @@ const RoutesComponent = () => (
       <Switch>
         <PublicRoute path="/auth" component={Auth} layout={AuthLayout} />
         <PublicRoute path="/kahoots" component={Kahoot} layout={KahootLayout} />
-        {/* <PublicRoute
+        <PublicRoute
           path="/creator"
           component={Creator}
           layout={CreatorLayout}
-        /> */}
+        />
         <PublicRoute
           path="/play-game"
           component={PlayGame}
