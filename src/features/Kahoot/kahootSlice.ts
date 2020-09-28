b8e813975bf1e1c3d4199acec66e03eccf3f7f47
malburo <tongquocbao0605@gmail.com>
2020-09-28 23:12:45 +0700

    create kahootSlice.ts

---
+++ src/features/Kahoot/kahootSlice.ts
@@ -0,0 +1,100 @@
+/* eslint-disable no-underscore-dangle */
+import kahootApi, { kahootType } from '@/api/kahootApi';
+import { KahootFormValues } from '@/components/Form/KahootForm';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const getKahoots = createAsyncThunk(
+  'kahoot/getAll',
+  async (payload, thunkAPI) => {
+    try {
+      const kahootList = await kahootApi.getAll();
+      return kahootList.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.data);
+    }
+  },
+);
+export const createKahoot = createAsyncThunk(
+  'kahoot/create',
+  async (payload: KahootFormValues, thunkAPI) => {
+    try {
+      const newKahoot = await kahootApi.createKahoot(payload);
+      return newKahoot.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.data);
+    }
+  },
+);
+export const deleteKahoot = createAsyncThunk(
+  'kahoot/delete',
+  async (payload: string, thunkAPI) => {
+    try {
+      const newKahoot = await kahootApi.deleteKahoot(payload);
+      return newKahoot.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.data);
+    }
+  },
+);
+interface typeInitState {
+  kahoots: kahootType[];
+  isFetching: boolean;
+}
+const initialState: typeInitState = {
+  kahoots: [],
+  isFetching: false,
+};
+const kahootSlice = createSlice({
+  name: 'kahoot',
+  initialState,
+  reducers: {},
+  extraReducers: builder => {
+    builder.addCase(getKahoots.pending, (state, action) => {
+      state.isFetching = true;
+    });
+    builder.addCase(
+      getKahoots.fulfilled,
+      (state, { payload }: { payload: kahootType[] }) => {
+        state.isFetching = false;
+        state.kahoots = payload;
+      },
+    );
+    builder.addCase(getKahoots.rejected, (state, action) => {
+      state.isFetching = false;
+    });
+
+    builder.addCase(createKahoot.pending, (state, action) => {
+      state.isFetching = true;
+    });
+    builder.addCase(
+      createKahoot.fulfilled,
+      (state, { payload }: { payload: kahootType }) => {
+        state.isFetching = false;
+        state.kahoots.push(payload);
+      },
+    );
+    builder.addCase(createKahoot.rejected, (state, action) => {
+      state.isFetching = false;
+    });
+
+    builder.addCase(deleteKahoot.pending, (state, action) => {
+      state.isFetching = true;
+    });
+    builder.addCase(
+      deleteKahoot.fulfilled,
+      (state, { payload }: { payload: kahootType }) => {
+        const index = state.kahoots.findIndex(
+          kahoot => kahoot._id === payload._id,
+        );
+        state.kahoots.splice(index, 1);
+        state.isFetching = false;
+      },
+    );
+    builder.addCase(deleteKahoot.rejected, (state, action) => {
+      state.isFetching = false;
+    });
+  },
+});
+
+const { reducer: kahootReducer } = kahootSlice;
+export default kahootReducer;
