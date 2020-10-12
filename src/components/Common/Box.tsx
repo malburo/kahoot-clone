1d62b514a27b8f78f20f5ea520255626d4a6564e
malburo <tongquocbao0605@gmail.com>
2020-10-12 18:34:30 +0700

    create AnswerCard.tsx and update Box.tsx

--- src/components/Common/Box.tsx
+++ src/components/Common/Box.tsx
@@ -8,13 +8,30 @@ import {
   LayoutProps,
   shadow,
   ShadowProps,
+  border,
+  BorderProps,
+  flexbox,
+  FlexboxProps,
+  position,
+  PositionProps,
 } from 'styled-system';
 
-const Box = styled.div<SpaceProps & ColorProps & LayoutProps & ShadowProps>`
+const Box = styled.div<
+  SpaceProps &
+    ColorProps &
+    LayoutProps &
+    ShadowProps &
+    BorderProps &
+    FlexboxProps &
+    PositionProps
+>`
   ${space}
   ${color}
   ${layout}
   ${shadow}
+  ${border}
+  ${flexbox}
+  ${position}
 `;
 
 export default Box;
